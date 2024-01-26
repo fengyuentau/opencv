34f8a5305c0385b70d0d451587970e41b718443f
fengyuentau <yuantao.feng@opencv.org.cn>
2024-01-26 16:22:27 +0800

    handle warnings

--- modules/dnn/src/layers/cpu_kernels/fast_gemm_kernels.simd.hpp
+++ modules/dnn/src/layers/cpu_kernels/fast_gemm_kernels.simd.hpp
@@ -313,7 +313,7 @@ static inline void fast_gemm12x16_f32(int k, const char *a_, const char *b_, cha
     const float* b = (const float*)b_;
     float* c = (float*)c_;
 
-    __m256i dummy;
+    __m256i dummy{0};
     __m256 s00  = (__m256)__lasx_xvxor_v(dummy, dummy), s01  = s00,
            s10  = s00, s11  = s00,
            s20  = s00, s21  = s00,

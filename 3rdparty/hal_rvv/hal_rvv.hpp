ad5b837d697dcef26e03a4f875a189ca71750aba
fengyuentau <yuantao.feng@outlook.com>
2025-03-10 16:16:59 +0800

    added rvv hal magnitude

--- 3rdparty/hal_rvv/hal_rvv.hpp
+++ 3rdparty/hal_rvv/hal_rvv.hpp
@@ -38,6 +38,8 @@
 #include "hal_rvv_1p0/cholesky.hpp" // core
 #include "hal_rvv_1p0/qr.hpp" // core
 #include "hal_rvv_1p0/svd.hpp" // core
+#include "hal_rvv_1p0/magnitude.hpp" // core
+
 
 #include "hal_rvv_1p0/pyramids.hpp" // imgproc
 #include "hal_rvv_1p0/color.hpp" // imgproc

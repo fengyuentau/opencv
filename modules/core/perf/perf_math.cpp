fdd982b1acba850b938d275beff2f7b87593a54f
fengyuentau <yuantao.feng@outlook.com>
2025-03-18 15:55:42 +0800

    base 4.11: added perf cases for magnitude

--- modules/core/perf/perf_math.cpp
+++ modules/core/perf/perf_math.cpp
@@ -80,6 +80,27 @@ PERF_TEST_P(CartToPolarFixture, CartToPolar,
     SANITY_CHECK_NOTHING();
 }
 
+///////////// Magnitude /////////////
+
+typedef Size_MatType MagnitudeFixture;
+
+PERF_TEST_P(MagnitudeFixture, Magnitude,
+    testing::Combine(testing::Values(TYPICAL_MAT_SIZES), testing::Values(CV_32F, CV_64F)))
+{
+    cv::Size size = std::get<0>(GetParam());
+    int type = std::get<1>(GetParam());
+
+    cv::Mat x(size, type);
+    cv::Mat y(size, type);
+    cv::Mat magnitude(size, type);
+
+    declare.in(x, y, WARMUP_RNG).out(magnitude);
+
+    TEST_CYCLE() cv::magnitude(x, y, magnitude);
+
+    SANITY_CHECK_NOTHING();
+}
+
 // generates random vectors, performs Gram-Schmidt orthogonalization on them
 Mat randomOrtho(int rows, int ftype, RNG& rng)
 {
